a181dc8fe0fb829c1aed3a34c588a4233b73ed22
Habibu Haruna Durumi <89111257+habibu-HD@users.noreply.github.com>
2022-08-16 06:44:08 +0100

    Update Location.js
    Corrected a typo error in location

--- components/Location.js
+++ components/Location.js
@@ -10,7 +10,7 @@ const Location = () => {
       <div className='flex flex-col lg:flex-row justify-between'>
         <div className='bg-green-800 w-full lg:max-w-[400px] my-4 lg:mr-10 flex justify-center items-center font-ralewaMedium leading-10 text-white tracking-wider '>
           <p className='p-4 lg:p-0 text-center lg:text-left'>
-            Number 32, Hadiza Hourse Behind <br /> Jifatu Plaza, Zoo Road,
+            Number 32, Hadiza House Behind <br /> Jifatu Plaza, Zoo Road,
             <br />
             Kano Kano <br />
             State, Nigeria
