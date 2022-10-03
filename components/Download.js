38dbff193909a6e33e054a46ef64d43124384a3e
Habibu Haruna Durumi <89111257+habibu-HD@users.noreply.github.com>
2022-10-03 08:47:37 +0100

    Update Download.js
    changed the download button and added links

--- components/Download.js
+++ components/Download.js
@@ -1,25 +1,23 @@
+
+
 /** @format */
 
-import React from "react";
-import Image from "next/image";
+import { useState } from "react";
+import DownloadLinks from "./utils/DownloadLinks";
 
-const Download = ({ img }) => {
+const Download = () => {
+  const [showDownload, setShowDownload] = useState(false);
   return (
-    <div className='mx-4 md:mx-8'>
-      <h2 className='lead my-6'>download our app</h2>
-      <div className='flex justify-between md:justify-center items-center z-0'>
-        {img.map((image, i) => (
-          <span
-            className='mx-4 hover:cursor-pointer hover:scale-105 transition-all duration-500'
-            key={i}>
-            <a href={image.url}>
-              <Image src={image.name} width={200} height={60} alt='img' />
-            </a>
-          </span>
-        ))}
-      </div>
+    <div className='mx-4 md:mx-8 my-8'>
+      <h2
+        onClick={() => setShowDownload(true)}
+        className='lead my-6 bg-red-700 text-white w-1/2 mx-auto py-6   hover:cursor-pointer hover:scale-110 duration-500 ease-in-out'>
+        download now
+      </h2>
+      {showDownload && <DownloadLinks setShowDownload={setShowDownload} />}
     </div>
   );
 };
 
 export default Download;
+
